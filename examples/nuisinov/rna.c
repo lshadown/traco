fb84d1bb9dd71f6b81ad543856f8b23d2d8e2f02
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2015-09-23 17:46:40 +0000

    examples
    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@27 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

---
+++ examples/nuisinov/rna.c
@@ -0,0 +1,55 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<omp.h>
+
+#define max(x,y)    ((x) > (y)? (x) : (y))
+
+int N = 100;
+
+int can_pair(float *input, int a, int b){
+   return ((((input[a] + input[b]) == 3) && (a < b - 1))) ? 1 : 0;
+}
+
+int main(int argc, char *argv[])
+{
+    int num_proc=1;
+    num_proc = atoi(argv[1]);
+    int kind;
+
+    if(argc > 2)
+        N = atoi(argv[2]);
+
+    int DIM = 2*N+2;
+
+    if(argc > 3)
+        kind = atoi(argv[3]);
+
+    int i,j,k;
+    double start, end;
+
+
+    float (*S)[DIM];
+    S = (float (*)[DIM])malloc(DIM * DIM * sizeof(float));
+    float *RNA;
+    RNA = (float (*))malloc(DIM * sizeof(float));
+
+
+    start = omp_get_wtime();
+
+    for (i = N-1; i >= 0; i--) {
+     for (j = i+1; j < N; j++) {
+      for (k = 0; k < j-i; k++) {
+         S[i][j] = max(S[i][k+i] + S[k+i+1][j], S[i][j]); // s0
+      }
+      S[i][j] = max(S[i][j], S[i+1][j-1] +can_pair(RNA, i, j));
+     }
+    }
+
+    end = omp_get_wtime();
+	printf("%.3f\n", end - start);
+
+    return 0;
+
+
+}
+
