f920804ed6a6954429bbf74e7bfa3f3eb738cb6e
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2015-11-25 08:15:54 +0000

    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@60 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

--- examples/cana16/FT_auxfnct.f2p_2.c
+++ examples/cana16/FT_auxfnct.f2p_2.c
@@ -48,6 +48,21 @@ for (c0 = 0; c0 <= floord(N1 - 1, 16); c0 += 1)
             x[c4][c3][c5]=y[c4][c3][c5];
           }
 
+/*
+
+#pragma omp parallel for
+for (c0 = 0; c0 <= floord(N1 - 1, 16); c0 += 1)
+  for (c1 = 0; c1 <= floord(N2 - 1, 16); c1 += 1)
+    for (c2 = 0; c2 <= floord(N3 - 1, 16); c2 += 1)
+    for (c5 = 16 * c0 + 1; c5 <= min(N1, 16 * c0 + 16); c5 += 1)
+      for (c3 = 16 * c1 + 1; c3 <= min(N2, 16 * c1 + 16); c3 += 1)
+        for (c4 = 16 * c2 + 1; c4 <= min(N3, 16 * c2 + 16); c4 += 1)
+          {
+            y[c4][c3][c5]=y[c4][c3][c5]*twiddle[c4][c3][c5];
+            x[c4][c3][c5]=y[c4][c3][c5];
+          }
+        */
+
 
 }
 
