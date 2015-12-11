0023abeb9675a349c1fa35dd4b6a45f752e3df39
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2015-12-11 11:21:14 +0000

    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@72 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

--- py/test.py
+++ py/test.py
@@ -1,5 +1,13 @@
 import re
 
+try:
+    import islpy as isl
+except ImportError, e:
+    print e
+    print "pip install ispy"
+    sys.exit()
+    ctx = isl.Context()
+
 line = "i+ii-2*i"
 
 operators = ["i", "ii"]
@@ -9,3 +17,21 @@ for w in operators:
     line = re.sub(pattern, w + "'", line)
 
 print line
+
+r = isl.Map("{[i,j] -> [i',j'] : ( i' = 1+i and j' = 1+j  and 0 <= i <= 2 and 0 <= j <= 2 ) or ( i' = 1+i and 1+j' = j  and 0 <= i <= 2 and 1 <= j <= 3 ) or (i=0) };");
+
+
+#printer = isl.Printer.to_str(isl.DEFAULT_CONTEXT)
+#printer = printer.set_output_format(3)
+#printer.print_map(r)
+#print(printer.get_str())
+
+b = isl.AstBuild.from_context(isl.Set("{:}"))
+m = isl.Map("{S[i]->[i]: 0 <= i <= 10}")
+ast = b.ast_from_schedule(m)
+print ast
+p = isl.Printer.to_str(isl.DEFAULT_CONTEXT)
+p = p.set_output_format(isl.format.C)
+p.flush() 
+p = p.print_ast_node(ast)
+print(p.get_str()) 
