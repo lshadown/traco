b995bc9811b5722296bb7744251657531cb7689e
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2015-12-11 11:52:02 +0000

    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@73 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

--- py/test.py
+++ py/test.py
@@ -18,20 +18,22 @@ for w in operators:
 
 print line
 
-r = isl.Map("{[i,j] -> [i',j'] : ( i' = 1+i and j' = 1+j  and 0 <= i <= 2 and 0 <= j <= 2 ) or ( i' = 1+i and 1+j' = j  and 0 <= i <= 2 and 1 <= j <= 3 ) or (i=0) };");
+r = isl.Map("{[i,j] -> [i',j'] : ( i' = 1+i and j' = 1+j  and 0 <= i <= 2 and 0 <= j <= 2 ) or ( i' = 1+i and 1+j' = j  and 0 <= i <= 2 and 1 <= j <= 3 ) or (Exists a: (2a = i)) };");
 
 
-#printer = isl.Printer.to_str(isl.DEFAULT_CONTEXT)
-#printer = printer.set_output_format(3)
-#printer.print_map(r)
-#print(printer.get_str())
+printer = isl.Printer.to_str(isl.DEFAULT_CONTEXT)
+printer = printer.set_output_format(3)
+printer.print_map(r)
+print(printer.get_str())
 
 b = isl.AstBuild.from_context(isl.Set("{:}"))
-m = isl.Map("{S[i]->[i]: 0 <= i <= 10}")
+m = isl.Map("{S[i,j]->[i,j]: 0 <= i <= 10 && 0 <= j <= 10}")
 ast = b.ast_from_schedule(m)
-print ast
 p = isl.Printer.to_str(isl.DEFAULT_CONTEXT)
 p = p.set_output_format(isl.format.C)
 p.flush() 
 p = p.print_ast_node(ast)
 print(p.get_str()) 
+
+S = isl.BasicSet("{[i,j]: Exists a: (2*a = i  && 0 <= j <= 10)}")
+print S.get_constraints()
