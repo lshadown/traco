87ecf52a7e685649761af35fe1ccfbec2c3edd12
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2015-12-21 07:12:04 +0000

    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@81 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

--- examples/cana16/UA_diffuse.f2p_5.c
+++ examples/cana16/UA_diffuse.f2p_5.c
@@ -37,35 +37,49 @@ int k, iz, i, j;
 void comp(float u[DIM1][DIM2][DIM3], float wdtdr[DIM1][DIM2], float r[DIM1][DIM2][DIM3]) {
 int c0,c1,c2,c3,c4,c5,c6,c7,c8;
 {
-if (N1 >= 17)
-  for (c0 = 0; c0 <= (N1 - 1) / 16; c0 += 1)
-    if (c0 >= 1) {
+
+int UB = min(floord(N2 - 1, 16) + 1, floord(N4 - 1, 16));
+int UB2 = floord(N2 - 1, 16);
+
+if (N1 >= 1)
 #pragma omp parallel for
-      for (c2 = 0; c2 <= min(floord(N2 - 1, 16), floord(N4 - 1, 16)); c2 += 1)
-        for (c3 = 0; c3 <= min(floord(N3 - 1, 16), (N2 - 1) / 16); c3 += 1)
-          for (c4 = 0; c4 <= min((N4 - 1) / 16, (N3 - 1) / 16); c4 += 1)
-            for (c5 = 16 * c0 + 1; c5 <= min(16 * c0 + 16, N1); c5 += 1)
-              for (c6 = 16 * c3 + 1; c6 <= min(N3, 16 * c3 + 16); c6 += 1)
-                for (c7 = 16 * c4 + 1; c7 <= min(16 * c4 + 16, N4); c7 += 1)
-                  for (c8 = 16 * c2 + 1; c8 <= min(N2, 16 * c2 + 16); c8 += 1)
-                    r[c7][c6][c8]=r[c7][c6][c8]+u[c7][c6][c5]*wdtdr[c5][c8];
-    } else
+  for (c1 = 0; c1 <= UB; c1 += 1)
+    for (c2 = 0; c2 <= min(floord(N2 - 1, 16), floord(N3 - 1, 16) + 1); c2 += 1)
+      for (c3 = 0; c3 <= min((N4 - 1) / 16 + 1, floord(N3 - 1, 16)); c3 += 1)
+          for (c0 = 0; c0 <= floord(N1 - 1, 16); c0 += 1)
+            for (c4 = 16 * c0 + 1; c4 <= min(16 * c0 + 16, N1); c4 += 1)
+              for (c5 = 16 * c3 + 1; c5 <= min(min(N4 + 16, N3), 16 * c3 + 16); c5 += 1)
+                for (c6 = 16 * c1 + 1; c6 <= min(min(N2 + 16, 16 * c1 + 16), N4); c6 += 1)
+                  for (c7 = 16 * c2 + 1; c7 <= min(min(N3 + 16, N2), 16 * c2 + 16); c7 += 1)
+                    r[c6][c5][c7]=r[c6][c5][c7]+u[c6][c5][c4]*wdtdr[c4][c7];
+
+
+/*
+
+
 #pragma omp parallel for
-      for (c2 = 0; c2 <= min(floord(N2 - 1, 16), floord(N4 - 1, 16)); c2 += 1)
-        for (c3 = 0; c3 <= min(floord(N3 - 1, 16), (N2 - 1) / 16); c3 += 1)
-          for (c4 = 0; c4 <= min((N4 - 1) / 16, (N3 - 1) / 16); c4 += 1)
-            for (c5 = 1; c5 <= 16; c5 += 1)
-              for (c6 = 16 * c3 + 1; c6 <= min(N3, 16 * c3 + 16); c6 += 1)
-                for (c7 = 16 * c4 + 1; c7 <= min(16 * c4 + 16, N4); c7 += 1)
-                  for (c8 = 16 * c2 + 1; c8 <= min(N2, 16 * c2 + 16); c8 += 1)
-                    r[c7][c6][c8]=r[c7][c6][c8]+u[c7][c6][c5]*wdtdr[c5][c8];
+  for (c1 = 0; c1 <= UB2; c1 += 1)
+    for (c2 = 0; c2 <= floord(N3 - 1, 16); c2 += 1)
+      for (c3 = 0; c3 <= floord(N4 - 1, 16); c3 += 1)
+         for (c0 = 0; c0 <= floord(N1 - 1, 16); c0 += 1)
+            for (c4 = 16 * c0 + 1; c4 <= min(16 * c0 + 16, N1); c4 += 1)
+              for (c5 = 16 * c1 + 1; c5 <= min(16 * c1 + 16, N2); c5 += 1)
+                for (c6 = 16 * c2 + 1; c6 <= min(16 * c2 + 16, N3); c6 += 1)
+                  for (c7 = 16 * c3 + 1; c7 <= min(16 * c3 + 16, N4); c7 += 1)
+                    r[c7][c6][c5]=r[c7][c6][c5]+u[c7][c6][c4]*wdtdr[c4][c5];
 
 
-}
-
-
-
-
+#pragma omp parallel for
+    for(k = 1; k <= N1; k++){
+          for(i = 1; i <= N4; i++){
+            for(j = 1; j <= N3; j++){
+              for(iz = 1; iz <= N2; iz++){
+              r[i][j][iz] = r[i][j][iz]+u[i][j][k]*wdtdr[k][iz];
+          }
+        }
+      }
+    }
+*/
 }
 
 
@@ -117,32 +131,4 @@ int main(int argc, char *argv[]) {
 
 }
 
-
-/*
-
-if (N1 >= 17)
-  for (c0 = 0; c0 <= (N1 - 1) / 16; c0 += 1)
-    if (c0 >= 1) {
-#pragma omp parallel for
-      for (c2 = 0; c2 <= floord(N2 - 1, 16); c2 += 1)
-        for (c3 = 0; c3 <= floord(N3 - 1, 16); c3 += 1)
-          for (c4 = 0; c4 <= floord(N4 - 1, 16); c4 += 1)
-            for (c5 = 16 * c0 + 1; c5 <= min(16 * c0 + 16, N1); c5 += 1)
-              for (c6 = 16 * c2 + 1; c6 <= min(16 * c2 + 16, N2); c6 += 1)
-                for (c7 = 16 * c3 + 1; c7 <= min(16 * c3 + 16, N3); c7 += 1)
-                  for (c8 = 16 * c4 + 1; c8 <= min(16 * c4 + 16, N4); c8 += 1)
-                    r[c8][c7][c6]=r[c8][c7][c6]+u[c8][c7][c5]*wdtdr[c5][c6];
-    } else
-#pragma omp parallel for
-      for (c2 = 0; c2 <= floord(N2 - 1, 16); c2 += 1)
-        for (c3 = 0; c3 <= floord(N3 - 1, 16); c3 += 1)
-          for (c4 = 0; c4 <= floord(N4 - 1, 16); c4 += 1)
-            for (c5 = 1; c5 <= 16; c5 += 1)
-              for (c6 = 16 * c2 + 1; c6 <= min(16 * c2 + 16, N2); c6 += 1)
-                for (c7 = 16 * c3 + 1; c7 <= min(16 * c3 + 16, N3); c7 += 1)
-                  for (c8 = 16 * c4 + 1; c8 <= min(16 * c4 + 16, N4); c8 += 1)
-                    r[c8][c7][c6]=r[c8][c7][c6]+u[c8][c7][c5]*wdtdr[c5][c6];
-
-
-*/
 
