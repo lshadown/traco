4998257a0b0db9d3643955c9d5caf9a158a79818
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2015-12-21 08:05:32 +0000

    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@83 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

--- examples/cana16/ua_adapt_10.c
+++ examples/cana16/ua_adapt_10.c
@@ -234,6 +234,23 @@ for (c0 = 1; c0 <= N1; c0 += 1)
 
 
     }
+/*
+#pragma omp parallel for
+for(ix = 1; ix <= N2; ix++){
+for(iz = 1; iz <= N1; iz++){
+    y[ix][1][iz]=x1[ix][1][iz];
+    y[ix][2][iz]=0;
+    for(ip = 1; ip <= N3; ip++){
+      y[ix][2][iz]=y[ix][2][iz]+map2[ip]*x1[ix][ip][iz];
+    }
+    y[ix][3][iz]=x1[ix][lx1][iz];
+    y[ix][4][iz]=0;
+    for(ip = 1; ip <= N4; ip++){
+      y[ix][4][iz]=y[ix][4][iz]+map4[ip]*x2[ix][ip][iz];
+    }
+    y[ix][lx1][iz]=x2[ix][lx1][iz];
+  }
+}
 */
 
 
