75173b0f9dff36406447ca99be7e26eec97bb0e4
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2016-01-05 13:51:36 +0000

    fsnew
    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@105 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

--- py/fs_new.py
+++ py/fs_new.py
@@ -48,3 +48,15 @@ def fs_new(rel, rel_plus, uds, LPetit, dane, plik, SIMPLIFY, rap, acc, loop):
     rlex = "{[" + ",".join(inp) + "] -> [" + ",".join(outp) + "] : " + tiling_v3.CreateLex(outp, inp) + "}"
     rlex = isl.Map(rlex)
 
+    rip = rel_plus.fixed_power_val(-1)
+
+    re = rlex.subtract(rel_plus).subtract(rip).coalesce()
+
+
+
+    print re
+
+
+
+
+
