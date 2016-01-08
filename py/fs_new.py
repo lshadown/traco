8344e3daac7fb2ca66b620afc3d5f4740d1a55f8
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2016-01-08 12:24:24 +0000

    fsnew
    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@106 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

--- py/fs_new.py
+++ py/fs_new.py
@@ -21,6 +21,7 @@ import clanpy
 def fs_new(rel, rel_plus, uds, LPetit, dane, plik, SIMPLIFY, rap, acc, loop):
 
 
+
     # R = R - R+ compose R
 
     rel = rel.subtract(rel_plus.apply_range(rel))
@@ -33,7 +34,14 @@ def fs_new(rel, rel_plus, uds, LPetit, dane, plik, SIMPLIFY, rap, acc, loop):
     cl.Load()
     cl.RunCandl()
 
-    print isl.Set(cl.statements[0].domain_map).coalesce()
+    IS =  isl.Set(cl.statements[0].domain_map).coalesce()
+
+    set_size = IS.dim(isl.dim_type.set)
+    IS = IS.insert_dims(isl.dim_type.set, set_size, 1)
+    IS = IS.set_dim_name(isl.dim_type.set, set_size, "v")
+
+    c= isl.Constraint.eq_from_names(IS.get_space(), {"v": -1, 1:int(dane[0])})
+    IS = IS.add_constraint(c).coalesce()
 
     n = rel.dim(isl.dim_type.in_)
 
@@ -50,11 +58,37 @@ def fs_new(rel, rel_plus, uds, LPetit, dane, plik, SIMPLIFY, rap, acc, loop):
 
     rip = rel_plus.fixed_power_val(-1)
 
-    re = rlex.subtract(rel_plus).subtract(rip).coalesce()
+    re_rel = isl.Map.from_domain_and_range(IS, IS)
+
+    #print re
+
+    print "###RE"
+
+    re_rel = re_rel.intersect(rlex.subtract(rel_plus).subtract(rip)).coalesce()
+
+    print re_rel
+
+    W = re_rel.domain().union(re_rel.range()).coalesce()
+    D = re_rel.domain().subtract(re_rel.range()).coalesce()
+
+    REPR = D.union(IS.subtract(W)).coalesce()
+
+    print "REPR"
+    print REPR
+
+    rel_inv = rel.fixed_power_val(-1)
+
+    print "R^-1"
+    print rel_inv
 
 
+    # R = R compose RINV
 
-    print re
+    RR = rel.apply_range(rel_inv)
+
+    print "RR"
+    print RR
+
 
 
 
