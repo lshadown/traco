6b4442bea6a836e70eb2232716be57438020aee9
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2016-01-23 15:26:07 +0000

    cana
    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@147 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

--- examples/schedulerp/bada/floyd.cpp
+++ examples/schedulerp/bada/floyd.cpp
@@ -75,39 +75,70 @@ if (N >= 1) {
 }else{
 
     //traco
-    int c0,c1,c2,c3,c4,c5;
+    int c0,c1,c2,c3,c4,c5,c6,c8,l;
+    int UB1 = (N - 1) / 16;
+    int UB = min(c0 + l / 16 - 1, -c0 + l / 16 + 1);
 
-for (c0 = 0; c0 <= floord(N - 1, 32); c0 += 1)
-  for (c1 = 0; c1 <= (N - 1) / 32; c1 += 1)
-    for (c2 = 0; c2 <= (N - 1) / 32; c2 += 1)
-      if (N >= 32 * c2 + 33) {
-        for (c4 = 32 * c1; c4 <= min(32 * c1 + 31, N - 1); c4 += 1)
-          for (c5 = 32 * c2; c5 <= 32 * c2 + 31; c5 += 1)
-            path[c4][c5]=path[c4][c5]<path[c4][32*c0]+path[32*c0][c5]?path[c4][c5]:path[c4][32*c0]+path[32*c0][c5];
+for(l=0; l < N ; l++){
+  if (N >= l + 1 && l >= 34)
+    for (c0 = 0; c0 <= min(3, N - l + 1); c0 += 1)
+      if (c0 <= 2) {
+        if (N >= l + c0)
+#pragma omp parallel for
+          for (c2 = 0; c2 <= UB; c2 += 1)
+            if (c0 == 2) {
+              for (c4 = l / 16 + 1; c4 <= (N - 1) / 16; c4 += 1)
+                for (c6 = 16 * c2; c6 <= 16 * c2 + 15; c6 += 1)
+                  for (c8 = 16 * c4; c8 <= min(N - 1, 16 * c4 + 15); c8 += 1)
+                    path[c6][c8]=path[c6][c8]<path[c6][l]+path[l][c8]?path[c6][c8]:path[c6][l]+path[l][c8];
+            } else if (c0 == 0) {
+              for (c4 = 0; c4 < l / 16; c4 += 1)
+                for (c6 = 16 * c2; c6 <= 16 * c2 + 15; c6 += 1)
+                  for (c8 = 16 * c4; c8 <= 16 * c4 + 15; c8 += 1)
+                    path[c6][c8]=path[c6][c8]<path[c6][l]+path[l][c8]?path[c6][c8]:path[c6][l]+path[l][c8];
+            } else if (l >= 16 * c2 + 16) {
+              for (c6 = 16 * c2; c6 <= 16 * c2 + 15; c6 += 1)
+                for (c8 = -(l % 16) + l; c8 <= min(-(l % 16) + l + 15, N - 1); c8 += 1)
+                  path[c6][c8]=path[c6][c8]<path[c6][l]+path[l][c8]?path[c6][c8]:path[c6][l]+path[l][c8];
+            } else
+              for (c4 = 0; c4 < l / 16; c4 += 1)
+                for (c6 = 16 * c2; c6 <= min(N - 1, 16 * c2 + 15); c6 += 1)
+                  for (c8 = 16 * c4; c8 <= 16 * c4 + 15; c8 += 1)
+                    path[c6][c8]=path[c6][c8]<path[c6][l]+path[l][c8]?path[c6][c8]:path[c6][l]+path[l][c8];
+        if (c0 == 2)
+#pragma omp parallel for
+          for (c2 = l / 16; c2 <= UB1; c2 += 1)
+            if (16 * c2 >= l + 1) {
+              for (c4 = 0; c4 < l / 16; c4 += 1)
+                for (c6 = 16 * c2; c6 <= min(16 * c2 + 15, N - 1); c6 += 1)
+                  for (c8 = 16 * c4; c8 <= 16 * c4 + 15; c8 += 1)
+                    path[c6][c8]=path[c6][c8]<path[c6][l]+path[l][c8]?path[c6][c8]:path[c6][l]+path[l][c8];
+            } else
+              for (c6 = 16 * c2; c6 <= min(N - 1, 16 * c2 + 15); c6 += 1)
+                for (c8 = 16 * c2; c8 <= min(N - 1, 16 * c2 + 15); c8 += 1)
+                  path[c6][c8]=path[c6][c8]<path[c6][l]+path[l][c8]?path[c6][c8]:path[c6][l]+path[l][c8];
       } else
-        for (c3 = 32 * c0; c3 <= min(N - 1, 32 * c0 + 31); c3 += 1)
-          for (c4 = 32 * c1; c4 <= min(N - 1, 32 * c1 + 31); c4 += 1) {
-            if (c3 >= 32 * c0 + 1)
-              for (c5 = 0; c5 < 32 * c2; c5 += 1)
-                path[c4][c5]=path[c4][c5]<path[c4][c3]+path[c3][c5]?path[c4][c5]:path[c4][c3]+path[c3][c5];
-            for (c5 = 32 * c2; c5 < N; c5 += 1)
-              path[c4][c5]=path[c4][c5]<path[c4][c3]+path[c3][c5]?path[c4][c5]:path[c4][c3]+path[c3][c5];
-          }
+#pragma omp parallel for
+        for (c2 = l / 16; c2 <= UB1; c2 += 1)
+          if (16 * c2 >= l + 1) {
+            for (c6 = 16 * c2; c6 <= min(N - 1, 16 * c2 + 15); c6 += 1)
+              for (c8 = -(l % 16) + l; c8 <= -(l % 16) + l + 15; c8 += 1)
+                path[c6][c8]=path[c6][c8]<path[c6][l]+path[l][c8]?path[c6][c8]:path[c6][l]+path[l][c8];
+          } else
+            for (c4 = c2 + 1; c4 <= (N - 1) / 16; c4 += 1)
+              for (c6 = 16 * c2; c6 <= 16 * c2 + 15; c6 += 1)
+                for (c8 = 16 * c4; c8 <= min(16 * c4 + 15, N - 1); c8 += 1)
+                  path[c6][c8]=path[c6][c8]<path[c6][l]+path[l][c8]?path[c6][c8]:path[c6][l]+path[l][c8];
+  if (l >= 34)
+#pragma omp parallel for
+    for (c2 = l / 16 + 1; c2 <= UB1; c2 += 1)
+      for (c4 = l / 16 + 1; c4 <= (N - 1) / 16; c4 += 1)
+        for (c6 = 16 * c2; c6 <= min(16 * c2 + 15, N - 1); c6 += 1)
+          for (c8 = 16 * c4; c8 <= min(N - 1, 16 * c4 + 15); c8 += 1)
+            path[c6][c8]=path[c6][c8]<path[c6][l]+path[l][c8]?path[c6][c8]:path[c6][l]+path[l][c8];
+}
 
- // simplify
- /*
-for (c0 = 0; c0 <= floord(N - 1, 32); c0 += 1)
-  for (c1 = 0; c1 <= (N - 1) / 32; c1 += 1) {
-    for (c2 = 0; c2 <= (N - 1) / 32; c2 += 1)
-      for (c4 = 32 * c1; c4 <= min(32 * c1 + 31, N - 1); c4 += 1)
-        for (c5 = 32 * c2; c5 <= min(N - 1, 32 * c2 + 31); c5 += 1)
-          path[c4][c5]=path[c4][c5]<path[c4][32*c0]+path[32*c0][c5]?path[c4][c5]:path[c4][32*c0]+path[32*c0][c5];
-    for (c3 = 32 * c0 + 1; c3 <= min(32 * c0 + 31, N - 1); c3 += 1)
-      for (c4 = 32 * c1; c4 <= min(32 * c1 + 31, N - 1); c4 += 1)
-        for (c5 = 0; c5 < N; c5 += 1)
-          path[c4][c5]=path[c4][c5]<path[c4][c3]+path[c3][c5]?path[c4][c5]:path[c4][c3]+path[c3][c5];
-  }
-  */
+
 }
 
     double end = omp_get_wtime();
