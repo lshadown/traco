eaaf8e9147333b1ba3c2c751bf0aaeb6827ff2c3
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2016-01-26 09:19:41 +0000

    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@192 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

--- examples/schedulerp/bada/trisolv32.cpp
+++ examples/schedulerp/bada/trisolv32.cpp
@@ -24,37 +24,24 @@ int main(int argc, char *argv[]) {
 	int k,l,i,j,t,loop=N,n=N;
 
 	// Declare arrays on the stack
-	double **za = new double*[N+1];
-	double **za1 = new double*[N+1];
-	double **zb = new double*[N+1];
-	double **zr = new double*[N+1];
-	double **zu = new double*[N+1];
-	double **zv = new double*[N+1];
-	double **zz = new double*[N+1];
+	double **B = new double*[N+1];
+	double **B1 = new double*[N+1];
+	double **L = new double*[N+1];
+
 
 	for (i=0; i<N+1; i++){
-	  za[i] = new double[N+1];
-	  za1[i] = new double[N+1];
-	  zb[i] = new double[N+1];
-	  zr[i] = new double[N+1];
-	  zu[i] = new double[N+1];
-	  zv[i] = new double[N+1];
-	  zz[i] = new double[N+1];
-
-
+	  B[i] = new double[N+1];
+	  B1[i] = new double[N+1];
+	  L[i] = new double[N+1];
     }
-    double *w = new double[N];
+
 
 	// Set the input data
 	for (i=0; i<N+1; i++) {
 		for (j=0; j<N+1; j++) {
-			za[i][j] = 0.001;
-			za1[i][j] = 0.001;
-			zb[i][j] = 0.002;
-			zr[i][j] = 0.003;
-			zv[i][j] = 0.005;
-			zu[i][j] = 0.002;
-			zz[i][j] = 0.001;
+			B[i][j] = 0.001*(i+1)*j;
+			B1[i][j] = 0.001*(i+1)*j;
+			L[i][j] = 0.001*(i+1)*j;
 		}
 	}
 
@@ -77,15 +64,16 @@ if(kind == 1){
 #pragma endscop
 }
 
-
-}else{
+else{
 
     //traco
 
-int c0,c1,c2,c3,c4,c5c6,c7,c8,c10,c12;
+int c0,c1,c2,c3,c4,c5,c6,c7,c8,c10,c12,c9,c11;
+int UB = floord(N - 1, 16);
+
 
 #pragma omp parallel for
-for (c1 = 0; c1 <= floord(N - 1, 16) + 1; c1 += 1)
+for (c1 = 0; c1 <= UB + 1; c1 += 1)
   for (c3 = 0; c3 <= floord(N - 1, 16); c3 += 1) {
     for (c5 = c3; c5 <= min(c3 + 1, (N + 14) / 16); c5 += 1)
       for (c7 = max(16 * c3, 16 * c5 - 15); c7 <= min(min(N - 1, 16 * c3 + 15), 16 * c5); c7 += 1)
@@ -103,33 +91,30 @@ for (c1 = 0; c1 <= floord(N - 1, 16) + 1; c1 += 1)
       }
   }
 
-
-
-
-
 }
-    double end = omp_get_wtime();
-	printf("%.3f\n", end - start);
+    double end1 = omp_get_wtime();
+	printf("%.3f\n", end1 - start);
 
 
     if(kind != 1){
-    for ( l=1 ; l<=loop ; l++ ) {
-        for ( j=1 ; j<6 ; j++ ) {
-            for ( k=1 ; k<n ; k++ ) {
-                za1[j][k] = za1[j][k] + 0.175*( za1[j+1][k]*zr[j][k] + za1[j-1][k]*zb[j][k] + za1[j][k+1]*zu[j][k] + za1[j][k-1]*zv[j][k] + zz[j][k] - za1[j][k] );
+    for (i=0;i<=N-1;i++) {
+        for (j=0;j<=N-1;j++) {
+            for (k=0;k<=j-1;k++) {
+                B1[j][i]=B1[j][i]-L[j][k]*B1[k][i];
+
             }
+            B1[j][i]=B1[j][i]/L[j][j];
         }
     }
 
-    for ( l=1 ; l<=loop ; l++ ) {
-        for ( j=1 ; j<6 ; j++ ) {
-            for ( k=1 ; k<n ; k++ ) {
-              if(za[j][k] != za1[j][k])
-                printf("Error!\n %.f %.f\n", za[j][k], za1[j][k]);
+    for (i=0;i<=N-1;i++) {
+        for (j=0;j<=N-1;j++) {
+              if(B[j][k] != B1[j][k])
+                printf("Error!\n %.f %.f\n", B[i][j], B1[i][j]);
                 exit(0);
               }
         }
-    }
+
     }
 
 
