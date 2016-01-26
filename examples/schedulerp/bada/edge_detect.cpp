3c03fa677f7f54af17fca71f3f772d130c2c27ab
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2016-01-26 07:14:08 +0000

    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@191 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

--- examples/schedulerp/bada/edge_detect.cpp
+++ examples/schedulerp/bada/edge_detect.cpp
@@ -1,7 +1,13 @@
+/* This program detects the edges image.
+   The program relies on a 2D-convolution routine to convolve the image with
+   kernels (Sobel operators) that expose horizontal and vertical edge
+   information. UTDSP */
+
 #include <omp.h>
 #include <math.h>
 #include <stdio.h>
 #include<stdlib.h>
+
 
 #define ceild(n,d)  ceil(((double)(n))/((double)(d)))
 #define floord(n,d) floor(((double)(n))/((double)(d)))
@@ -11,37 +17,69 @@
 
 
 
-int N = 1024;
+int N = 100;
 
-// This is 'seidel-2d', a 2D Seidel stencil computation
+
+
 int main(int argc, char *argv[]) {
 
     int kind = atoi(argv[1]);
 	int N = atoi(argv[2]);
+	int K = 30;
 	int cpus = atoi(argv[3]);
 	int z = 0;
 
 	omp_set_num_threads(cpus);
 
-	int k,i,j,t;
+	int k,i,j,t,r,c;
+
+    int dead_rows = K / 2;
+    int dead_cols = K / 2;
+    int normal_factor;
 
 	// Declare arrays on the stack
-	float **path = new float*[N];
-	for (i=0; i<N; i++)
-	  path[i] = new float[N];
+	int **sum = new int*[N];
+	int **sum1 = new int*[N];
+	int **kernel = new int*[N];
+	int **input_image = new int*[2*N];
+	int **output_image = new int*[2*N];
+	int **output_image1 = new int*[2*N];
 
-    float **path1 = new float*[N];
-	for (i=0; i<N; i++)
-	  path1[i] = new float[N];
+	for (i=0; i<N; i++) { sum[i] = new int[2*N]; }
+    for (i=0; i<2*N; i++) { sum1[i] = new int[2*N]; }
+	for (i=0; i<N; i++) { kernel[i] = new int[N]; }
+	for (i=0; i<2*N; i++) { input_image[i] = new int[2*N]; }
+    for (i=0; i<2*N; i++) { output_image[i] = new int[2*N]; }
+	for (i=0; i<2*N; i++) { output_image1[i] = new int[2*N]; }
+
 
 	// Set the input data
 	for (i=0; i<N; i++) {
 		for (j=0; j<N; j++) {
-			//path[i][j] = ((float) (N-j)*j*(j+2) + 2) / N;
-			path[i][j] = rand()/N;
-			path1[i][j] =  path[i][j]; //  ((float) (N-j)*j*(j+2) + 2) / N;
+			sum[i][j] = rand()/N;
+			sum1[i][j] =  sum[i][j];
+			kernel[i][j] = rand()/N;
 		}
 	}
+
+    // Set the input data
+	for (i=0; i<N; i++) {
+		for (j=0; j<N; j++) {
+			output_image[i][j] = rand()/N;
+			output_image1[i][j] =  output_image[i][j];
+        	input_image[i][j] = rand()/N;
+		}
+	}
+
+
+    normal_factor = 0;
+    for (r = 0; r < K; r++) {
+        for (c = 0; c < K; c++) {
+      normal_factor += abs(kernel[r][c]);
+        }
+    }
+
+
 
 double start = omp_get_wtime();
 // -- sekw.
@@ -60,37 +98,20 @@ if(kind == 1){
     }
   }
 }
-}
+
 else if(kind == 3){
 
 
-  int t1, t2, t3, t4, t5;
 
- register int lbv, ubv;
-
-/* Start of CLooG code */
-if (N >= 1) {
-  for (t1=0;t1<=N-1;t1++) {
-    for (t2=0;t2<=floord(N-1,32);t2++) {
-      for (t3=0;t3<=floord(N-1,32);t3++) {
-        for (t4=32*t2;t4<=min(N-1,32*t2+31);t4++) {
-          for (t5=32*t3;t5<=min(N-1,32*t3+31);t5++) {
-            path[t4][t5] = path[t4][t5] < path[t4][t1] + path[t1][t5] ? path[t4][t5] : path[t4][t1] + path[t1][t5];;
-          }
-        }
-      }
-    }
-  }
-}
 }
 else{
 
     //traco
-    int c0,c1,c2,c3,c4,c5,c6,c8,c7.c9.c11,c13,c15
-    ;
+    int c0,c1,c2,c3,c4,c5,c6,c8,c7,c9,c11,c13,c15;
 
+int UB = floord(N - K, 16);
 #pragma omp parallel for
-for (c1 = 0; c1 <= floord(N - K, 16); c1 += 1)
+for (c1 = 0; c1 <= UB; c1 += 1)
   for (c3 = 0; c3 <= (N - K) / 16; c3 += 1) {
     if (K >= 1) {
       if (K >= 17)
@@ -101,7 +122,7 @@ for (c1 = 0; c1 <= floord(N - K, 16); c1 += 1)
         for (c11 = 16 * c3; c11 <= min(N - K, 16 * c3 + 15); c11 += 1) {
           if (K <= 16)
             sum[c9][c11]=0;
-          output_image[c9+dead_rows][c11+dead_cols]=(sum[c9][c11]/normal_factor];
+          output_image[c9+dead_rows][c11+dead_cols]=(sum[c9][c11]/normal_factor);
         }
     } else
       for (c9 = 16 * c1; c9 <= min(N - K, 16 * c1 + 15); c9 += 1)
@@ -136,24 +157,26 @@ for (c1 = 0; c1 <= floord(N - K, 16); c1 += 1)
 	printf("%i\n", z);
 
     if(kind != 1){
-        for (k = 0; k < N; k++)
-        {
-          for(i = 0; i < N; i++)
-            for (j = 0; j < N; j++){
-              path1[i][j] = path1[i][j] < path1[i][k] + path1[k][j] ? path1[i][j] : path1[i][k] + path1[k][j];
-
-              }
+      for (r = 0; r < N - K + 1; r++) {
+        for (c = 0; c < N - K + 1; c++) {
+          sum1[r][c] = 0;
+          for (i = 0; i < K; i++) {
+            for (j = 0; j < K; j++) {
+              sum1[r][c] += input_image[r+i][c+j] * kernel[i][j];
+            }
+          }
+          output_image1[r+dead_rows][c+dead_cols] = (sum1[r][c] / normal_factor);
         }
+      }
 
-        for (k = 0; k < N; k++)
-        {
-          for(i = 0; i < N; i++)
-            for (j = 0; j < N; j++){
-              if(path1[i][j] != path[i][j])
+
+          for(i = 0; i < 2*N; i++)
+            for (j = 0; j < 2*N; j++){
+              if(output_image[i][j] != output_image1[i][j])
                 printf("Error!\n");
                 exit(0);
               }
-        }
+
     }
 
 
