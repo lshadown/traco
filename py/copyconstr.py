6e76df61e4f45ff538ea6ad1641d7f636f2c48b8
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2016-04-18 10:10:43 +0000

    tracovis
    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@273 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

--- py/copyconstr.py
+++ py/copyconstr.py
@@ -1,29 +1,46 @@
 import islpy as isl
 import relation_util
 
+
+def GetConstr(list_in, list_out, rel):   #lista zmiennych wej, lista zmiennych wyj, relacja
+
+    sep = '] :'
+    conj_sep = 'union'
+
+    rel = isl.Map(rel)
+
+
+    for i in range(0, len(list_in)):
+        rel = rel.set_dim_name(isl.dim_type.in_, i, list_in[i])
+
+    for i in range(0, len(list_out)):
+        rel = rel.set_dim_name(isl.dim_type.out, i, list_out[i])
+
+
+    z = relation_util.islMaptoOmegaStr(rel)
+
+
+    conj_list = z.split(conj_sep)
+
+    constraints = ''
+
+    for c in conj_list:
+        item = c.split(sep)[1]
+        constraints = constraints + " ( " +  item  + " ) or "
+
+    constraints = constraints.replace('}', '')
+    constraints = constraints[:-3]
+
+    constraints = " ( " + constraints + " ) "
+
+    return constraints
+
+
+
 rel = "{ [i, j, 6] -> [i', j', 6] : i >= 0 and j >= 8 + i and i' <= 9 and j <= j' <= 9 and j' <= 7 + i'; [i, 5, 6] -> [i', j', 6] : i >= 0 and i' <= 9 and 7 + i <= j' <= 9 and j' <= 7 + i'; [i, j, 6] -> [i', 5, 6] : i >= 0 and 8 + i <= j <= 9 and -7 + j <= i' <= 9; [i, j, 6] -> [i', j, 6] : i >= 0 and 0 <= j <= 9 and i < i' <= 9; [i, j, v] -> [i, j, v] }"
 
-rel = isl.Map(rel)
+in_ = ["_i0", "_i1", "_i2"]
+out_ = ["_o0", "_o1", "_o2"]
 
 
-rel = rel.set_dim_name(isl.dim_type.in_, 0, "_i0")
-rel = rel.set_dim_name(isl.dim_type.in_, 1, "_i1")
-rel = rel.set_dim_name(isl.dim_type.in_, 2, "_i2")
-
-rel = rel.set_dim_name(isl.dim_type.out, 0, "_o0")
-rel = rel.set_dim_name(isl.dim_type.out, 1, "_o1")
-rel = rel.set_dim_name(isl.dim_type.out, 2, "_o2")
-
-print(rel)
-
-set_test = '[_x0,_x1,_x2]->{[_x0,_x1,_x2]}'
-set_test = isl.Set(set_test)
-
-print set_test
-
-
-print relation_util.islMaptoOmegaStr(rel)
-
-rel_test = set_test.apply(rel)
-
-print relation_util.islSettoOmegaStr(rel_test)
+print GetConstr(in_, out_, rel)
