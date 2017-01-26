776308434c2995c4c8d6a6c16f29b27391928e03
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2017-01-26 11:01:24 +0000

    lu
    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@484 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

---
+++ examples/rna/rtt/zuker.c
@@ -0,0 +1,78 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <omp.h>
+
+
+#define min(a,b) (((a)<(b))?(a):(b))
+#define MIN(a,b) (((a)<(b))?(a):(b))
+#define max(a,b) (((a)>(b))?(a):(b))
+#define floord(n,d) floor(((double)(n))/((double)(d)))
+#define ceild(n,d) ceil(((double)(n))/((double)(d)))
+
+long double **W;
+long double **V;
+long double **EFL;
+long double **EHF;
+
+int N = 100, DIM = 102;
+
+#include "zuker_oryg.h"
+#include "zuker_pluto.h"
+#include "zuker_traco.h"
+#include "mem.h"
+
+
+
+int main(int argc, char *argv[]){
+
+    int i,j,k;
+
+
+    int num_proc=1;
+    num_proc = atoi(argv[1]);
+
+    int kind;
+
+    if(argc > 2)
+        N = atoi(argv[2]);
+    DIM = N+2;
+
+
+    if(argc > 3)
+        kind = atoi(argv[3]);
+
+    W = mem();
+    V = mem();
+    EFL = mem();
+    EHF = mem();
+
+
+    for(i=0; i<N; i++)
+     for(j=0; j<N; j++){
+     W[i][j] = i*j*0.001;
+     V[i][j] = i*j*0.004;
+    }
+
+
+
+    double start = omp_get_wtime();
+
+    if(kind == 3){
+        //printf("!!!\n");
+        zuker_traco();
+    }
+
+    if(kind == 2)
+        zuker_pluto();
+
+    if(kind == 1)
+        zuker_seq();
+
+    double stop = omp_get_wtime();
+    printf("%.2f\n",stop - start);
+
+    omp_set_num_threads(4);
+
+
+return 0;
+}
