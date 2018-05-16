262c558620c6210b49ba52f11085f0dd96dc6719
mpalkowski <mpalkowski@a6dccb1e-4862-4600-a7b9-2890f7aa3ab7>
2018-05-16 09:05:32 +0000

    else
    git-svn-id: http://svn.code.sf.net/p/traco/code/trunk@519 a6dccb1e-4862-4600-a7b9-2890f7aa3ab7

---
+++ py/time_space_tiling.py
@@ -0,0 +1,90 @@
+import os, glob
+import re
+import sys
+import time
+from termcolor import colored
+
+import numpy
+try:
+    import islpy as isl
+except ImportError, e:
+    print e
+    print "pip install ispy"
+    sys.exit()
+
+import convert_loop
+import Dependence
+
+
+def tile(plik, block, permute, output_file="", L="0", SIMPLIFY="False", perfect_mode = False, parallel_option = False, rplus_mode = '', cpus=2):
+
+    print ''
+    print colored('/\__  _\ /\  == \   /\  __ \   /\  ___\   /\  __ \   ', 'green')
+    print colored('\/_/\ \/ \ \  __<   \ \  __ \  \ \ \____  \ \ \/\ \  ', 'green')
+    print colored('   \ \_\  \ \_\ \_\  \ \_\ \_\  \ \_____\  \ \_____\ ', 'green')
+    print colored('    \/_/   \/_/ /_/   \/_/\/_/   \/_____/   \/_____/ ', 'green')
+    print ''
+    print '      An Automatic Parallelizer and Optimizer'
+    print 'based on the '+colored('TRA','green')+'nsitive '+colored('C', 'green')+'l'+colored('O', 'green')+'sure of dependence graphs'
+    print '             traco.sourceforge.net               '
+    print ''
+    print 'TIME SPACE TILING Module'
+
+
+    LPetit = "tmp/tmp_petit"+L+".t"
+
+    BLOCK = block.split(',')
+
+    for i in range(len(BLOCK),10):
+        BLOCK.append(BLOCK[len(BLOCK)-1])
+
+    BLOCK2 = [0,6,6]
+    # BLOCK2 = BLOCK
+
+
+    linestring = open(plik, 'r').read()
+    lines = linestring.split('\n')
+
+    petit_loop = convert_loop.convert_loop(lines)
+
+
+
+    file = open(LPetit, 'w')
+
+    imperf = 0
+    endloop = 0
+    startloop = 0
+    for line in petit_loop:
+        #sprawdz przy okazji jaka petla idealnie czy nie
+        if 'for' in line and not 'endfor' in line:
+            if startloop == 2:
+                imperf = 1
+            startloop = 1
+        else:
+            if startloop == 1:
+                startloop = 2
+        if 'endfor' in line:
+            endloop = 1
+        if endloop == 1 and 'endfor' not in line and not line.isspace() and line != '':
+            imperf = 1
+
+        file.write(line + '\n')
+
+    file.close()
+
+
+    start = time.time()
+    loop = Dependence.Kernel_Loop(LPetit,1)
+
+    loop.Load_Deps()
+    loop.Load_instrukcje()
+    loop.PreprocessPet()
+    loop.Get_Arrays()
+    end = time.time()
+    elapsed = end - start
+
+
+    print "Dependence analysis: time taken: ", elapsed, "seconds."
+
+    print colored('R', 'green')
+    print loop.isl_rel
